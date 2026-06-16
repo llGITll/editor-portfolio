f41ee23ea4cb08c8cdbf0d3c6deb271826cecaf0
Dharmik Vaja <142163369+llGITll@users.noreply.github.com>
2026-06-16 20:54:52 +0530

    Update projectsData.js

--- javascript/projectsData.js
+++ javascript/projectsData.js
@@ -135,6 +135,30 @@ const projectsData = {
       thumbnail: 'https://res.cloudinary.com/drpu4slkk/image/upload/v1781621851/Screenshot_2026-06-16_202717_cagjsv.png',
       video: 'https://www.youtube.com/embed/uTcVCoN6_Pg',
       type: 'youtube'
+    },
+    {
+      id: 5,
+      title: 'Voxel Institute Brand Campaign',
+      description: 'Educational marketing video',
+      thumbnail: '',
+      video: '',
+      type: 'youtube'
+    },
+    {
+      id: 6,
+      title: 'Popular Jewellers Collection Showcase',
+      description: 'Promotional video featuring premium silver jewellery collections.',
+      thumbnail: 'https://res.cloudinary.com/drpu4slkk/image/upload/v1781623064/WhatsApp_Image_2026-06-16_at_8.46.30_PM_m8pgek.jpg',
+      video: 'https://www.youtube.com/embed/DTllnHE9Y54',
+      type: 'youtube'
+    },
+    {
+      id: 6,
+      title: 'Hearty Brand',
+      description: 'Product advertisement showcasing a premium streetwear baseball cap.',
+      thumbnail: 'https://res.cloudinary.com/drpu4slkk/image/upload/v1781623316/WhatsApp_Image_2026-06-16_at_8.50.53_PM_yrjxoj.jpg',
+      video: 'https://www.youtube.com/embed/DTllnHE9Y54',
+      type: 'youtube'
     }
   ],
   
